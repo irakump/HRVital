df0eda3a6049017346866b74b50d97fec3809084
Ira <ira.kumpula@metropolia.fi>
2025-05-02 17:35:54 +0300

    Add kubios and history, delete comments

--- project_files/main.py
+++ project_files/main.py
@@ -1,28 +1,30 @@
 from oled import Encoder, Oled
-from heart_rate import HR
+#from heart_rate import HR # vanha hr-tiedosto
+from HR_ppg_signal import HR # uusi hr-tiedosto
 import time
-#from basic_hrv_analysis import BasicHRVAnalysis # original
-from basic_hrv_analysis import BasicHRVAnalysis # uusi testi
-#from mqtt import Mqtt
+from basic_hrv_analysis import BasicHRVAnalysis
 from menu import Menu
+from kubios import Kubios
+from history import History
 
 # Results for testing
 #hrv_results = [77, 1000, 23, 22]
-kubios_result = ['30.4.2025  12:59', 77, 1000, 23, 22, -1.1, 1.9] # aikaleima max levyinen
-measurements = [['30.4.2025  12:59', 55, 1000, 23, 22, 0.5, 1.8], ['aikaleima', 88, 999, 33, 54, 2.0, -1.5], ['aikaleima', 72, 878, 29, 51, 1.0, -1.1]] # max pituus = 4!
+#kubios_result = ['30.4.2025  12:59', 77, 1000, 23, 22, -1.1, 1.9] # aikaleima max levyinen
+#measurements = [['30.4.2025  12:59', 55, 1000, 23, 22, 0.5, 1.8], ['aikaleima', 88, 999, 33, 54, 2.0, -1.5], ['aikaleima', 72, 878, 29, 51, 1.0, -1.1]] # max pituus = 4!
 #measurements = [] # test for no history
 
 # Definitions
+hr = HR()
 rot = Encoder()
 oled = Oled()
-hr = HR()
 hrv = BasicHRVAnalysis()
-#mqtt = Mqtt()
-menu = Menu(oled, rot, hr, hrv, measurements) # measurements-lista (kubios-mittauksesta!)
+kubios = Kubios()
+history = History()
+menu = Menu(oled, rot, hr, hrv, kubios, history)
 
-# TODO: hrv-mittauksen funktion sisään importaa rot (handler), poistuminen kesken mittauksen
-# oledista: stopping ja error texts näyttö, jos error tai hrv datan keräys epäonnistuu
-# uusi hr + pulse yhdistys menuun!
+
+# TODO:
+# oledista: error texts näyttö, jos error tai hrv datan keräys epäonnistuu?
 # jos ehtii, laskeva ajastin hrv-mittaukseen
 
 # kaikki tiedostot pitää tallentaa picolle, jotta import toimii
